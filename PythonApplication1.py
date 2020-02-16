ed9932a426e20d238804d6d9032890fc055b6f3f
kkkkkkkk591 <61118230+kkkkkkkk591@users.noreply.github.com>
2020-02-17 02:31:48 +0900

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -6,12 +6,7 @@ import json
 
 client = discord.Client()
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+
 
 @client.event
 async def on_message(message):
