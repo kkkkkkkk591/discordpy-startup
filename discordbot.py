955650f2dc7de3ab92ae8c2192c05ebc456a0b83
kkkkkkkk591 <61118230+kkkkkkkk591@users.noreply.github.com>
2020-02-17 01:40:59 +0900

    gg

--- discordbot.py
+++ discordbot.py
@@ -1,41 +1,22 @@
-import discord
-import asyncio
-import random
-import urllib.request
-import json
+from discord.ext import commands
+import os
+import traceback
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
 
-@client.event
-async def on_message(message):
-    # 「おはよう」で始まるか調べる
-    d = 0
-    a = 0
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
-    if message.content.startswith('K!pc'):
-     if client.user != message.author:
-        while a != 1:
-           a = random.randint(1, 4096)
-           d = d + 1
-           
-        i = message.author.name +"さんは色違いのポケモンに"+str(d)+"回目で遭遇しました。"
-        await message.channel.send(i)
 
-    if message.content.startswith('K!help'):
-        if client.user != message.author:
-         await message.channel.send("K!help・・・このメニュー\r\nK!pc・・・色違いと遭遇するまでの野生のポケモンとの遭遇回数計算")
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
-    if message.content.startswith('K!a'):
-        if client.user != message.author:
-         resp = urllib.request.urlopen('https://twitter.com/mokouliszt').read()
-         resp = json.loads(resp.decode('utf-8'))
-         await message.channel.send(resp[apple-mobile-web-app-title])
 
 bot.run(token)
+
